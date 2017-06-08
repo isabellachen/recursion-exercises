1e5c4a332b5aac195d3ece2ff37e21d9887f2ed1
Isa Chen <miss.izzyc@gmail.com>
2017-06-08 16:21:22 +0200

    more indentation/ formatting changes

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 function formula01 (memo) {
 let fib = function (n) {
-  	if (n === 1) {
+    if (n === 1) {
     	return memo
     } else {
     	memo = fib(n-1) //each iteration returns the memo array
@@ -16,18 +16,18 @@ console.log(findFib(8))
 
 //this only returns the value not the array
 function formula02 () {
-	let memo = [0,1]
-	let fib = function (n) {
-    if (n === 1) {
+    let memo = [0,1]
+    let fib = function (n) {	    
+     if (n === 1) {
     	return memo
-    }else{
+     }else{
     	memo = fib(n-1)
  	memo.push(memo[memo.length - 1] + memo[memo.length - 2])
  	console.log(n)
   	console.log(memo)
     	return memo
+     }	    
     }
-  }
   return fib
 }
 
